06c36a9778bd0d7031e4b4890ef40a11b25c61ac
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2013-08-03 14:57:13 -0400

    Imported scripts in index.html. Added basic object prototypes.

--- javascripts/objects.js
+++ javascripts/objects.js
@@ -1,3 +1,20 @@
 // Provides a global variable with a list of all objects
 // Defines each object and its associated variables
 
+function Object() {
+  this.mass = 1;
+  this.states = { start: new State(this), end: new State(this) }
+}
+
+// This will likely be a singleton
+function Environment() {
+  this.time = 0;
+}
+
+function State(obj) {
+  this.obj = obj
+  this.time = 0;
+  this.yPos = 0;
+  this.yVelocity = 0;
+  this.yAcceleration = 0;
+}
