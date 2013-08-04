4f24a039cb262ae515e7436e930b871151936d7e
Ray Zhou <rzhou186@stanford.edu>
2013-08-04 09:40:37 -0400

    Fixed clearing of console, answers, and backtrace

--- javascripts/controllers.js
+++ javascripts/controllers.js
@@ -253,5 +253,7 @@ canvasController.renderAll = function(){
 }
 
 consolesController.clear = function(){
-  $('.rightbar').children(".contents").html("")
+  $('.console').children(".contents").html("")
+  $('.answer').children(".contents").html("")
+  $('.backtrace').children(".contents").html("")
 }
