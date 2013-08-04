6b329d43d0c8819d05c178947b9e1329f6b7c3d6
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2013-08-04 04:11:12 -0400

    Updated kin_objects with vectorizing functions

--- javascripts/kin_objects.js
+++ javascripts/kin_objects.js
@@ -25,18 +25,36 @@ function KinObj(name) {
 
   // This is for drawn values; will append value to both dimensions
   this.addVector = function(name, val, angle) {
-    xComponent = val * Math.cos(convertToRadians(angle));
-    yComponent = val * Math.sin(convertToRadians(angle));
-
-    if (!_.find(this.knownVars, function(known) { return name === known; })) {
-      this.knownVars.push(name);
-      this.x[name] = xComponent;
-      this.y[name] = yComponent;
+    if (isNaN(val) {
+      this.x.knownVars.splice(this.x.knownVars.indexOf(name), 1);
+      delete this.x[name];
+      this.y.knownVars.splice(this.y.knownVars.indexOf(name), 1);
+      delete this.y[name];
     } else {
-      this.x[name] += xComponent;
-      this.y[name] += yComponent;
+      if (angle === undefined) {
+        xComponent = val;
+        yComponent = val;
+      } else {
+        xComponent = val * Math.cos(convertToRadians(angle));
+        yComponent = val * Math.sin(convertToRadians(angle));
+      }
+
+      if (!_.find(this.knownVars, function(known) { return name === known; })) {
+        this.x.knownVars.push(name);
+        this.x[name] = xComponent;
+        this.y.knownVars.push(name);
+        this.y[name] = yComponent;
+      } else {
+        this.x[name] += xComponent;
+        this.y[name] += yComponent;
+      }
     }
   };
+
+  this.get(varName, dim) {
+    if (dim)
+      return this[dim][varName];
+  }
 };
 
 function convertToRadians(degrees) {
