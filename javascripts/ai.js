da0de0320711f493eb2c7b30f5588bf4686c82f2
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2013-08-04 03:34:23 -0400

    First step to vectorizing

--- javascripts/ai.js
+++ javascripts/ai.js
@@ -1,41 +1,48 @@
-function computeFor(desiredVar, obj) {
+(function() {
+  var dimensions = ['x', 'y'];
 
-  // Check if the desired variable is already known
-  consolesController.logConsole("Checking if desired variable is already known...");
-  if (!isUnknown(desiredVar, obj)) {
-    consolesController.logConsole("Variable found. Terminating computation...");
-    return obj[desiredVar];
-  }
-  consolesController.logConsole("Variable not already known. Initiating computation...");
+  function computeFor(desiredVar, obj, dim) {
 
-  consolesController.logConsole("Initiating new cycle of formula computations...");
-  for (x in formulas) {
-    var formula = formulas[x];
-    var desiredVarIndex = -1;
-    for (i in formula.varNames) {
-      if (isUnknown(formula.varNames[i], obj)) {
-        if (desiredVar === formula.varNames[i]) {
-          desiredVarIndex = i;
-        } else {
-          desiredVarIndex = -1;
-          break;
+    // Check if the desired variable is already known
+    consolesController.logConsole("Checking if desired variable is already known...");
+    if (!isUnknown(desiredVar, obj)) {
+      consolesController.logConsole("Variable found. Terminating computation...");
+      return obj[desiredVar];
+    }
+    consolesController.logConsole("Variable not already known. Initiating computation...");
+
+    consolesController.logConsole("Initiating new cycle of formula computations...");
+    for (x in formulas) {
+      var formula = formulas[x];
+      var desiredVarIndex = -1;
+      for (i in formula.varNames) {
+        if (isUnknown(formula.varNames[i], obj)) {
+          if (desiredVar === formula.varNames[i]) {
+            desiredVarIndex = i;
+          } else {
+            desiredVarIndex = -1;
+            break;
+          }
         }
       }
+      if (desiredVarIndex === -1) {
+        continue;
+      }
+      consolesController.logBacktrace(formula.expression);
+      consolesController.logConsole("Solution found.");
+      return formula.functions[desiredVarIndex](obj)
+      break;
     }
-    if (desiredVarIndex === -1) {
-      continue;
-    }
-    consolesController.logBacktrace(formula.expression);
-    consolesController.logConsole("Solution found.");
-    return formula.functions[desiredVarIndex](obj)
-    break;
+
+    consolesController.logConsole("Computation unsuccessful.");
+    return "not known";
+
+  };
+
+  function isUnknown(varName, obj) {
+    return !_.find(obj.knownVars, function(knownVar) { return knownVar === varName; })
   }
 
-  consolesController.logConsole("Computation unsuccessful.");
-  return "not known";
-
-};
-
-function isUnknown(varName, obj) {
-  return !_.find(obj.knownVars, function(knownVar) { return knownVar === varName; })
-}
+  // Export computeFor
+  window.computeFor = computeFor;
+})();
--- javascripts/kin_objects.js
+++ javascripts/kin_objects.js
@@ -3,6 +3,8 @@ kinObjs = []
 function KinObj(name) {
   this.name = name;
   this.knownVars = [];
+  this.x = {};
+  this.y = {};
   this.setVar = function(name, val) {
     if (!_.find(this.knownVars, function(known) { return name === known; })) {
       this.knownVars.push(name);
@@ -15,5 +17,29 @@ function KinObj(name) {
     }
     else $('input'+"."+namesController.shortToReadable(name).replace(" ","-")).val(this[name])
   };
+
+  // This is for computed dimensional values; will only set on specified dimension
+  this.setVector = function(name, val, dim) {
+    this[dim][name] = val;
+  };
+
+  // This is for drawn values; will append value to both dimensions
+  this.addVector = function(name, val, angle) {
+    xComponent = val * Math.cos(convertToRadians(angle));
+    yComponent = val * Math.sin(convertToRadians(angle));
+
+    if (!_.find(this.knownVars, function(known) { return name === known; })) {
+      this.knownVars.push(name);
+      this.x[name] = xComponent;
+      this.y[name] = yComponent;
+    } else {
+      this.x[name] += xComponent;
+      this.y[name] += yComponent;
+    }
+  };
+};
+
+function convertToRadians(degrees) {
+  return (Math.PI / 180) * degrees);
 };
 
